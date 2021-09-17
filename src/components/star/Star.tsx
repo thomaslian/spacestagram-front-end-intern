739a61c2639fe7b84eaab6bd83946057a6c91b33
Thomas <thomaslio@live.no>
2021-09-17 20:19:45 +0200

    Created own function for button down

--- src/components/star/Star.tsx
+++ src/components/star/Star.tsx
@@ -13,14 +13,10 @@ export default class Star extends React.Component<Props, State> {
 
         this.state = { starred: false };
         this.handleClick = this.handleClick.bind(this);
+        this.handleButtonDown = this.handleButtonDown.bind(this);
     }
 
     handleClick(event: any): void {
-        // Keypresses other then Enter and Space should not trigger a command
-        if ((event.nativeEvent.pointerType !== "mouse" && event.nativeEvent.pointerType !== "touch") && event.key !== 'Enter' && event.key !== ' ') {
-            return;
-        }
-
         if (this.state.starred) {
             this.setState({ starred: false });
         } else {
@@ -28,10 +24,19 @@ export default class Star extends React.Component<Props, State> {
         }
     }
 
+    handleButtonDown(event: any) {
+        // Keypresses other then Enter and Space should not trigger a command
+        if (event.key !== 'Enter' && event.key !== ' ') {
+            return;
+        }
+
+        this.handleClick(event);
+    }
+
     render() {
         return (
             <div className={this.state.starred ? "Star starred" : "Star unstarred"} >
-                <FontAwesomeIcon className="star-icon far" onClick={this.handleClick} onKeyDown={this.handleClick} icon={faStar} role="button" tabIndex={0} />
+                <FontAwesomeIcon className="star-icon far" onClick={this.handleClick} onKeyDown={this.handleButtonDown} icon={faStar} role="button" tabIndex={0} />
             </div>
         );
     }
